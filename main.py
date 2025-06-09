57ca874ed9c052cf42e58ef8b9e34cfca237b18e
SoulOppen <arieloppenheimer@outlook.cl>
2025-06-08 21:54:38 -0400

    start:functions

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+from google.genai import types
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print("Not argument")
+        sys.exit(1)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    user_prompt = " ".join(args)
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    response=generateContent(client,messages)
+    if "--verbose" in args:
+        verbose(messages,response)
+def generateContent(client,messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
+    print(response.text)
+    return response
+def verbose(messages,response):
+    print(f"User prompt: {messages}")
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+if __name__ == "__main__":
+    main()
